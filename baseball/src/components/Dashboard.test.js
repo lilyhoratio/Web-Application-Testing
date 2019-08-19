3e55f25137209c876182b60b8a0bde269645f6f5
lilyhoratio <lilyhzhou@gmail.com>
2019-08-19 15:20:29 -0700

    tests for other buttons

--- baseball/src/components/Dashboard.test.js
+++ baseball/src/components/Dashboard.test.js
@@ -7,7 +7,7 @@ describe('<Dashboard />', () => {
     it('renders without crashing', () => {
         render(<Dashboard />)
     });
-    it('strike button', () => {
+    it('strike button onclick works', () => {
         let clicked = false
         const { getByText } = render(<Dashboard strike={() => clicked = true} />)
         // getByText('strike') 
@@ -15,13 +15,36 @@ describe('<Dashboard />', () => {
         fireEvent.click(strikeButton) // emulates user clicking on the button
         expect(clicked).toBe(true)
     })
-    it('strike button - using mock function', () => {
+    it('strike button on click works - using mock function', () => {
         let clicked = false
         const click = jest.fn() // pass as a prop 
         const { getByText } = render(<Dashboard strike={click} />)
-        // getByText('strike') 
-        const strikeButton = getByText(/strike/i) // regexp better in case dev alters text on button
-        fireEvent.click(strikeButton) // emulates user clicking on the button
+        const strikeButton = getByText(/strike/i)
+        fireEvent.click(strikeButton)
+        expect(click).toBeCalled()
+    })
+    it('ball button on click works - using mock function', () => {
+        let clicked = false
+        const click = jest.fn() // pass as a prop 
+        const { getByText } = render(<Dashboard ball={click} />)
+        const ballButton = getByText(/ball/i)
+        fireEvent.click(ballButton)
+        expect(click).toBeCalled()
+    })
+    it('foul button on click works - using mock function', () => {
+        let clicked = false
+        const click = jest.fn() // pass as a prop 
+        const { getByText } = render(<Dashboard foul={click} />)
+        const foulButton = getByText(/foul/i)
+        fireEvent.click(foulButton)
+        expect(click).toBeCalled()
+    })
+    it('hit button on click works - using mock function', () => {
+        let clicked = false
+        const click = jest.fn() // pass as a prop 
+        const { getByText } = render(<Dashboard hit={click} />)
+        const hitButton = getByText(/hit/i)
+        fireEvent.click(hitButton)
         expect(click).toBeCalled()
     })
 
